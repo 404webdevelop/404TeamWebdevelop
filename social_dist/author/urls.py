74c38c824525169de71cdad75cc3049836c12bce
Yushi Wang <jasper.ys.wang@gmail.com>
2016-02-13 15:29:22 -0700

    add author login logout

--- social_dist/author/urls.py
+++ social_dist/author/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='author/index.html')),
 
     url(r'^signup$', views.signup_view),
+    url(r'^login$', views.login_view),
+    url(r'^profile$', views.profile_view),
+    url(r'^logout$', views.logout),
 ]
--- social_dist/author/views.py
+++ social_dist/author/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods, require_GET, require_POST
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.template import loader
+import django.contrib.auth as auth
+
 
 
 @require_http_methods(['GET', 'POST'])
@@ -24,4 +28,41 @@ def signup(request):
     return redirect('/author')
 
 
-        
+@require_http_methods(['GET', 'POST'])
+def login_view(request):
+    if request.method == 'GET':
+        return redirect('/author')
+    elif request.method == 'POST':
+        return login(request)
+
+
+@require_POST
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = auth.authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            auth.login(request, user)
+            return redirect('/author/profile')
+        else:
+            return HttpResponse('login success, but user is not active')
+    else:
+        # TODO: make a class for this
+        res = HttpResponse("Unauthorized")
+        res.status_code = 401
+        return res
+
+
+@require_GET
+@login_required(login_url="/author")
+def profile_view(request):
+    return render(request, 'author/profile.html', {
+            'user': request.user
+        })
+
+
+@require_GET
+def logout(request):
+    auth.logout(request)
+    return redirect('/author')        
