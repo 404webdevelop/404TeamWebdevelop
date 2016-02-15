791af6f4cbe2f1cb2229bbffdf9ed24bc2d366d1
uaLogan <lglin@ualberta.ca>
2016-02-15 11:34:44 -0700

    models for Post, Image, Comment set up in post app

---
+++ social_dist/post/migrations/0001_initial.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('content', models.CharField(max_length=1000)),
+                ('date_created', models.DateTimeField()),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Image',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('file_type', models.CharField(max_length=50)),
+                ('image_data', models.BinaryField()),
+                ('date_created', models.DateTimeField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=300)),
+                ('content', models.CharField(max_length=5000)),
+                ('date_created', models.DateTimeField()),
+                ('last_modified', models.DateTimeField()),
+                ('privacy_level', models.CharField(max_length=10, choices=[(b'pub', b'Public'), (b'me', b'Private to me'), (b'friends', b'Friends only'), (b'fof', b'Friends of friends')])),
+                ('privacy_host_only', models.BooleanField()),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('privacy_whitelist', models.ManyToManyField(related_name='+', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='parent',
+            field=models.ForeignKey(to='post.Post'),
+        ),
+    ]
---
+++ social_dist/post/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length = 300)
+    content = models.CharField(max_length = 5000)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    date_created = models.DateTimeField()
+    last_modified = models.DateTimeField()
+
+    PERMISSIONS_CHOICES = (
+        ('pub', 'Public'),
+        ('me', 'Private to me'),
+        ('friends', 'Friends only'),
+        ('fof', 'Friends of friends'),
+    )
+    privacy_level = models.CharField(max_length = 10, choices = PERMISSIONS_CHOICES)
+    privacy_host_only = models.BooleanField() # if True, only users on this host may view
+    privacy_whitelist = models.ManyToManyField(User, related_name = '+') # Users on this list may always view
+
+    # TODO: one post one image? or just insert an embed code for the user maybe
+    # image = models.ForeignKey(Image, null=True, on_delete=models.SET_NULL)
+
+class Image(models.Model):
+    file_type = models.CharField(max_length = 50)
+    image_data = models.BinaryField()
+    date_created = models.DateTimeField()
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.CharField(max_length = 1000)
+    date_created = models.DateTimeField()
+
+    # TODO: do we want to allow editing of comments?
+    # last_modified = models.DateTimeField()
