49b015dcb6d6ce08b240a588103f5d4203d5bb1d
Yu Zuo <zuo2@cs.ualberta.ca>
2016-02-20 21:38:20 -0700

    user name switch

--- thjs.js
+++ thjs.js
@@ -1,10 +1,11 @@
-var username="YU"
+var username="Yu"
 
 if (username =="None"){
 	document.getElementById("welcomeitem").innerHTML ="<li class=\"active\"><a href=\"home.html\">Home</a></li><li><a href=\"about.html\">About us</a></li><li><a href=\"connect.html\">Connect</a></li>";
 	document.getElementById("loginbutton").innerHTML ="<li><button id=\"signup\"type=\"button\" class=\"btn btn-lg btn-primary\">Sign up</button></li> <li><span>fg</span></li>li><button id=\"signin\"type=\"button\" class=\"btn btn-lg btn-default\">Sign in</button></li>";
 }else{
-	document.getElementById("welcomeitem").innerHTML ="<li class=\"active\"><a href=\"home.html\">Home</a></li><li><a href=\"posted.html\">Posted</a></li><li><a href=\"connect.html\">Connect</a></li>";
-}
+	document.getElementById("welcomeitem").innerHTML ="<li class=\"active\"><a href=\"home.html\">Home</a></li><li><a href=\"posted.html\">Posted</a></li><li><a href=\"about.html\">About us</a></li><li><a href=\"connect.html\">Connect</a></li>";
+	document.getElementById("loginbutton").innerHTML ="<a href=\"profile.html\"><u>[ "+username+" ]</u></a><a href=\"profile.html\"> <img id=\"userimage\" src=\"image/Yu.jpg\"></a><button type=\"button\" class=\"btn btn-xs btn-warning\">Logout</button>";
+	}
 
 
