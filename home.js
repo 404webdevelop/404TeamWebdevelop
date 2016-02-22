0c0e99e84b16f43a5b0e1ada6c25d7a93b3ae62d
Yu Zuo <zuo2@ualberta.ca>
2016-02-22 02:50:41 -0700

    test

--- home.js
+++ home.js
@@ -9,6 +9,7 @@ function logout(){
 
 function signinbox(){
 
+
 }
 function signuppage(){
 	window.location="signup.html";
@@ -20,7 +21,7 @@ if (username == "None"){
 	document.getElementById("loginbutton").innerHTML ="<button id=\"signup\" onclick=\"signuppage()\"type=\"button\" class=\"btn btn-lg btn-primary\">Sign up</button> <a href=\"#myPopupDialog\" data-rel=\"popup\" data-position-to=\"window\" data-transition=\"fade\" ><button id=\"signin\"onclick=\"signinbox()\"type=\"button\" class=\"btn btn-lg btn-default\">Sign in</button></a>";
 }else{
 	document.getElementById("welcomeitem").innerHTML ="<li class=\"active\"><a href=\"home.html\">Home</a></li><li><a href=\"posted.html\">Posted</a></li><li><a href=\"about.html\">About us</a></li><li><a href=\"connect.html\">Connect</a></li>";
-	document.getElementById("loginbutton").innerHTML ="<a href=\"profile.html\"><u>[ "+username+" ]</u></a><a href=\"profile.html\"> <img id=\"userimage\" src=\"image/Yu.jpg\"></a><button id=\"logoutbutton\" type=\"button\" onclick=\"logout()\" class=\"btn btn-xs btn-warning\">Logout</button>";
+	document.getElementById("loginbutton").innerHTML ="<a href=\"profile.html\"><u>[ "+username+" ]</u></a><button id=\"logoutbutton\" type=\"button\" onclick=\"logout()\" class=\"btn btn-xs btn-warning\">Logout</button>";
 };
 
 
