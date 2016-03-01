6737e8da3787228f0dceb7ffe790a50208f026ae
uaLogan <lglin@ualberta.ca>
2016-03-01 13:02:10 -0700

    views require login now

--- social_dist/social_dist/urls.py
+++ social_dist/social_dist/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.contrib.auth.views import login
 from django.views.generic import TemplateView
 from rest_framework import routers
+from django.contrib.auth.decorators import login_required
 
 from post.views import PostViewSet, CommentViewSet, ImageViewSet
 from author.views import UserViewSet
@@ -18,14 +19,14 @@ urlpatterns = [
     # url(r'^$', 'social_dist.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name="home"),
-    url(r'^about$', TemplateView.as_view(template_name='about.html'), name="about"),
-    url(r'^addfriends$', TemplateView.as_view(template_name='addfriends.html'), name="addfriends"),
-    url(r'^connect$', TemplateView.as_view(template_name='connect.html'), name="connect"),
-    url(r'^createpost$', TemplateView.as_view(template_name='createpost.html'), name="createpost"),
-    url(r'^friends$', TemplateView.as_view(template_name='friends.html'), name="friends"),
-    url(r'^posted$', TemplateView.as_view(template_name='posted.html'), name="posted"),
-    url(r'^profile$', TemplateView.as_view(template_name='profile.html'), name="profile"),
+    url(r'^$', login_required(TemplateView.as_view(template_name='home.html')), name="home"),
+    url(r'^about$', login_required(TemplateView.as_view(template_name='about.html')), name="about"),
+    url(r'^addfriends$', login_required(TemplateView.as_view(template_name='addfriends.html')), name="addfriends"),
+    url(r'^connect$', login_required(TemplateView.as_view(template_name='connect.html')), name="connect"),
+    url(r'^createpost$', login_required(TemplateView.as_view(template_name='createpost.html')), name="createpost"),
+    url(r'^friends$', login_required(TemplateView.as_view(template_name='friends.html')), name="friends"),
+    url(r'^posted$', login_required(TemplateView.as_view(template_name='posted.html')), name="posted"),
+    url(r'^profile$', login_required(TemplateView.as_view(template_name='profile.html')), name="profile"),
 
     url(r'^admin/', include(admin.site.urls)),
 
