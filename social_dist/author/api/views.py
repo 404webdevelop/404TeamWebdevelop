aa98109ed2ed6ac73ad74b58bb3ad7baaf91a583
Yushi Wang <jasper.ys.wang@gmail.com>
2016-03-06 00:10:57 -0700

    add new api api/auhtors/me
    add its demo

--- social_dist/author/api/views.py
+++ social_dist/author/api/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication, TokenAuthentication, SessionAuthentication
-from rest_framework.decorators import detail_route
+from rest_framework.decorators import detail_route, list_route
 from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.models import Token
 from permissions import IsAdminOrSelfOrReadOnly
 from rest_framework.response import Response
 from serializers import UserSerializer
@@ -18,6 +20,8 @@ class AuthorViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action == 'create':
             self.permission_classes = [AllowAny, ]
+        if self.action == 'me':
+            self.permission_classes = [AllowAny,]
         return super(AuthorViewSet, self).get_permissions()
 
     def create(self, request, *args, **kwargs):
@@ -34,3 +38,17 @@ class AuthorViewSet(viewsets.ModelViewSet):
             user.save()
         serializer = UserSerializer(user, context={'request': request})
         return Response(serializer.data)
+
+    @list_route(methods=['POST'])
+    def me(self, request, **kwargs):
+        token_serializer = AuthTokenSerializer(data=request.data)
+        token_serializer.is_valid(raise_exception=True)
+        user = token_serializer.validated_data['user']
+        user_serializer = UserSerializer(user, context={'request': request})
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'user': user_serializer.data,
+            'token': token.key
+        })
+
+
--- social_dist/social_dist/urls.py
+++ social_dist/social_dist/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     url(r'^api-token/', views.obtain_auth_token),
 
     url(r'^$', login_required(TemplateView.as_view(template_name='home.html')), name="home"),
-    url(r'^signup$', TemplateView.as_view(template_name='user/signup.html'), name='signup'),
     url(r'^about$', login_required(TemplateView.as_view(template_name='about.html')), name="about"),
     url(r'^addfriends$', login_required(TemplateView.as_view(template_name='addfriends.html')), name="addfriends"),
     url(r'^connect$', login_required(TemplateView.as_view(template_name='connect.html')), name="connect"),
@@ -19,6 +18,11 @@ urlpatterns = [
     url(r'^friends$', login_required(TemplateView.as_view(template_name='friends.html')), name="friends"),
     url(r'^posted$', login_required(TemplateView.as_view(template_name='posted.html')), name="posted"),
     url(r'^profile$', login_required(TemplateView.as_view(template_name='profile.html')), name="profile"),
+
+    url(r'^demo-signup$', TemplateView.as_view(template_name='user/signup.html'), name='signup'),
+    url(r'^demo-update$', TemplateView.as_view(template_name='user/update.html'), name='update'),
+
+
 
     url(r'^admin/', include(admin.site.urls)),
 
