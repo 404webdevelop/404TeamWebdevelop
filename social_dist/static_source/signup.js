aab517b006650ef7d782bb463f5bf7cd568bfc94
zuoyu <zuo2@ualberta.ca>
2016-03-07 21:21:12 -0700

    sign up page works right now

--- social_dist/static_source/signup.js
+++ social_dist/static_source/signup.js
@@ -1,161 +1,63 @@
 
+(function () {
 
+'use strict';
 
-var data= {"username":getCookie("username"),
-		   "userphoto":"../static/image/Yu.jpg",
-		   "intro": "good luck"
-			 };
+var url = 'api/authors/'
 
-console.log(getCookie("username"));
-var post_item={
-		"post_title":"Lorem",
-		"post_text" :"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-		"post_autor":"hoho",
-		"post_date":"October 14, 2012",
-		"post_image":"/static/image/Yu.jpg"
-
-};
-
-
-var post_list=[];
-
-function setpostitem(title,text,image,date,autor){
-	post_item.post_title = title;
-	post_item.post_text = text;
-	post_item.post_image = image;
-	post_item.post_date = date;
-	post_item.post_autor=autor;
-	post_list.push(post_item);
-
-};
-
-
-setpostitem(post_item.post_title,post_item.post_text,post_item.post_image,post_item.post_date,post_item.post_autor);
-setpostitem(post_item.post_title,post_item.post_text,post_item.post_image,post_item.post_date,post_item.post_autor);
-setpostitem(post_item.post_title,post_item.post_text,post_item.post_image,post_item.post_date,post_item.post_autor);
-setpostitem(post_item.post_title,post_item.post_text,post_item.post_image,post_item.post_date,post_item.post_autor);
-
-
-
-
-
-
-function signuppage(){
-	window.location="signup";
+function postAuthor(author) {
+    console.log(author)
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: JSON.stringify(author),
+      contentType:"application/json; charset=utf-8",
+      success: function(msg){
+        alert( "successfull sign up your account please wait admin to prove" );
+        setTimeout(function(){
+          window.location.href = "home";
+          },1000
+        );     
+      },
+      error: function(error) {
+        alert( "Sorry the accout already exit. try another account" );
+      }
+    });
 }
 
-function setdynamic(img,tit,tex,date,author){
-	var string = "<li id=\"view_list_style\" class=\"ui-btn ui-li ui-li-has-thumb  ui-btn-up-c\"  ><div class=\"ui-btn-inner ui-li\"><div class=\"ui-btn-text\"><a class=\"ui-link-inherit\" href=\"#\"><img  style='height:2em;width:2em;' id=\"imagetag\"class=\"ui-li-thumb\" src=\""+img+"\"><p style='display:inline;float:left;position:relative;left:3em'> by "+author+"</p><h2 style='display:inline;' class=\"ui-li-heading\">\""+tit+"\"</a>&nbsp;&nbsp&nbsp;&nbsp&nbsp;&nbsp<span id=\"test\">["+date+"]</span></h2><p class=\"ui-li-desc\" style='white-space:normal;'>"+tex+"</p></a></div>&nbsp;</div></li>"
-	return string;
-};
-
-function setifor(img,username,intro){
-	var string = "<table><tr><td id = \"holder\"><center><img id = \"user_file_image\"src=\""+img+"\"height=\"150\" width=\"150\">  </center>  </td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td id=\"userintroduction\"><div class=\"page-header\"><h1>"+username+"</h1></div><div class=\"well\"><p>"+intro+"</p></div></td></tr></table>";
-	return string;
-};
-
-function loaddynamic(number){
-	var st="";
-	for(i = 0 ;i<number;i++){
-		st = st+setdynamic(post_list[i].post_image,post_list[i].post_title,post_list[i].post_text,post_list[i].post_date,post_list[i].post_autor);
-
-	}
-	return st
-};
-
-
-
-if (data.username == "undefined" || data.username == "" ){
-	
-	document.getElementById("loginbutton").innerHTML =" <a href=\"#myPopupDialog\" data-rel=\"popup\" data-position-to=\"window\" data-transition=\"fade\" ><button id=\"signin\"onclick=\"signinbox()\"type=\"button\" class=\"btn btn-lg btn-default\">Sign in</button></a>";
-	document.getElementById("connect-infor").innerHTML = "<div id=\"connect-infor\" class=\"alert alert-danger\" ><center><strong>You do not have access to view this page</strong><br>If you are not logged in, please do so now. </center></div>";
-	
-}else{
-	var number = post_list.length;
-	var st= loaddynamic(number)
-	var head = setifor(data.userphoto,data.username,data.intro)
-	
-	document.getElementById("loginbutton").innerHTML ="<a href=\"posted\" id=\"user_name_input\">[ "+data.username+" ]</a>&nbsp &nbsp &nbsp<a href=\"#myProfileDialog\" data-rel=\"popup\" data-position-to=\"window\" data-transition=\"fade\" ><button id=\"edit\"onclick=\"signinbox()\"type=\"button\" class=\"btn btn-lg btn-default\">Edit Profile</button></a><button id=\"logoutbutton\" type=\"button\" class=\"btn btn-lg btn-warning\">Logout</button>";
-	document.getElementById("info").innerHTML = head;
-	document.getElementById("connect-infor").innerHTML = "<div id=\"connect-infor\" class=\"alert alert-success\" ><center><strong>Successfully loaded!</strong> You can view your friends' posts below.<center><button id=\"Reloadbutton\" type=\"button\" onclick=\"javascript:history.go(0)\" class=\"btn btn-lg btn-success\">Refresh Feed</button> </div></div>";
-	document.getElementById("list_post_view").innerHTML = st;
-
-
-};
-
-function setCookie(key,value){
-  document.cookie = key+"="+value;
-  //document.cookie = "username = "+username;
-};
-
-
-function clearCookie(token){
-  setCookie(token,"undefined",-1);
+function getAuthor() {
+    return {
+        "username": value('#username-input') || 'user_99',
+        "password": value('#password-input') || '0000',
+        "email": value('#email-input') || "u1@email.com",
+        "author": {
+            "github": value('#github-input') || "user1"
+        },
+        "first_name": value('#first-name-input') || "Yushi",
+        "last_name": value('#last-name-input') || "Wang"
+    };
 }
 
-function getCookie(cname) {
-    var name = cname + "=";
-    //console.log(name);
-    var ca = document.cookie.split(';');
-    //console.log(ca);
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
+function value(field) {
+    return $(field).val();
 }
 
-function getlogin(url,data,callback){
-  var val;
-  var username = data.username;
-  //console.log(username);
-  var password = data.password;
-  var request = $.ajax({
-          method: "POST",
-          url: url,
-          data: data,
+function setup() {
+    $('#create-btn').click(function () {
+        var author = getAuthor();
+        postAuthor(author);   
 
-        });
-  request.done(function (callback) {
-            //console.log(callback);
-            var token =JSON.stringify(callback);
 
-            
-            setCookie("username",username);
-            setCookie("token",token);
-            
-         });
-  request.fail(function () {
-            //console.log(callback);
-            clearCookie("username");
-            clearCookie("token");
-            clearCookie("url");
-         });
-  //return callback;
+    });
 
-};
+}
 
-$("#login_submit").click(function(){
-    var username = $("#username").val();
-    var password = $("#password").val();
+setup();
 
-    var data = {"username": username, "password": password};
-    //console.log(data);
-    var url = "api-token/";
-    var callback = "";
-    getlogin(url,data,callback);
-    var token=getCookie("token");
-   
-    //console.log(token);
-    //$("#response").html(token);  
-    setTimeout(function(){
-      window.location.href = "home";
-    },1000
-      );
-});
+
+
+
+})();
 
 
 
