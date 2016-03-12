568a5f06b1c947ae19b1a25407500cdb7d8ae05e
Yushi Wang <jasper.ys.wang@gmail.com>
2016-03-12 16:51:07 +0000

    setup whitenoise

--- social_dist/social_dist/settings.py
+++ social_dist/social_dist/settings.py
@@ -15,6 +15,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # Quick-start development settings - unsuitable for production
