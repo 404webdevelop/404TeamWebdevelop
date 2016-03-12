f371eb5093fddbf39b1d29a7b1e5ea941931288e
Yushi Wang <jasper.ys.wang@gmail.com>
2016-03-12 18:02:19 +0000

    try agian

--- social_dist/social_dist/settings.py
+++ social_dist/social_dist/settings.py
@@ -104,7 +104,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
