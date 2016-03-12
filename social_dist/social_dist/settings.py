cd3817adb326f509ae2fc7a8634b16211ffa11df
Yushi Wang <jasper.ys.wang@gmail.com>
2016-03-12 09:03:00 +0000

    try

--- social_dist/social_dist/settings.py
+++ social_dist/social_dist/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = 'static_source/'
+STATIC_DIR = '../static_source/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
