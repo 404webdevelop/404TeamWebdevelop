b598cfbee2e7674b475b7b2583a63a1d2adc2e79
Yushi Wang <jasper.ys.wang@gmail.com>
2016-03-12 09:21:16 +0000

    try

--- social_dist/social_dist/settings.py
+++ social_dist/social_dist/settings.py
@@ -119,9 +119,6 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, STATIC_DIR),
-)
 
 # Authen & Login
 LOGIN_REDIRECT_URL = "/"
