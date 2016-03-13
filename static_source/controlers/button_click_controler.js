2b0f68d04e303995302dde46fc1f7a90373f975a
Yu Zuo <zuo2@ualberta.ca>
2016-03-13 03:13:30 -0600

    follow button done

--- static_source/controlers/button_click_controler.js
+++ static_source/controlers/button_click_controler.js
@@ -1,7 +1,35 @@
 (function (global) {
 'use strict';
 var cookie = global.cookie_setting;
+var boolean_value = "1";
+
+
+function checkfollowed(cookie,boolean_value,callback){      
+        var url = "api/follow/"+cookie.get("userid")+"/followings";
+        
+        
+        $.getJSON(url,function(data){
+            console.log(data);
+            $.each(data,function (i,value){
+              console.log(data[i].followed.split("/")[5]);
+              console.log("just: "+cookie.get("click_id"));
+              if(data[i].followed.split("/")[5] == cookie.get("click_id")){
+                console.log("wo de tiam");
+                $('#follow_btn').text("UNFOLLOW");
+                $('#follow_btn').css("background","#FF6347");
+                return false;
+              }else{
+                $('#follow_btn').text("FOLLOW");
+                $('#follow_btn').css("background","lightgreen");
+              }
+            });     
+         });
+      console.log(boolean_value);
+      }
+
+
 function button_click(cookie){
+    var value = checkfollowed(cookie,boolean_value);
 
     $('#list_post_view').delegate('li', 'click', function () {
          cookie.set("click_id",$(this).attr("value"));
@@ -13,8 +41,16 @@ function button_click(cookie){
 
      });
     $("#follow_btn").click(function(){
+      console.log($('#follow_btn').text());
+      if ($('#follow_btn').text() == "UNFOLLOW"){
+        $('#follow_btn').text("FOLLOW");
+        $('#follow_btn').css("background","lightgreen");
+      }else{
         $('#follow_btn').text("UNFOLLOW");
         $('#follow_btn').css("background","#FF6347");
+      }    
+
+
     });
 
     $('.target').delegate('li', 'click', function () {
