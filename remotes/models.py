a25721f47e3c8764565beab19304af60cd5fd491
lglin@ualberta.ca <example@gmail.com>
2016-03-18 22:36:57 -0600

    migrations

--- remotes/models.py
+++ remotes/models.py
@@ -3,11 +3,11 @@ from author.models import Author
 
 # Create your models here.
 class RemoteServer(models.Model):
-    hostname = models.CharField(max_length=2000)
+    hostname = models.CharField(max_length=2000) # e.g. 'someapp.herokuapp.com' or '127.0.0.1:8001'
 
-    local_user = models.ForeignKey(Author, blank=True, null=True)
+    local_user = models.ForeignKey(Author, blank=True, null=True) # credentials that they use to login to our server
 
-    remote_username = models.CharField(max_length=500, blank=True)
+    remote_username = models.CharField(max_length=500, blank=True) # credentials that we use to login to their server
     remote_password = models.CharField(max_length=200, blank=True)
 
     active = models.BooleanField(blank=True, default=True)
