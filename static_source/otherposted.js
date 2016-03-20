49ed12e5d93717a8db21a043c3a846de93c87332
Yu Zuo <zuo2@ualberta.ca>
2016-03-20 02:37:30 -0600

    otherpost jump done

--- static_source/otherposted.js
+++ static_source/otherposted.js
@@ -15,21 +15,6 @@ function setup(data){
         
         console.log("user id is:"+global.cookie_setting.get("userid"));
         console.log("target id is:"+global.cookie_setting.get("click_id"));
-        $.getJSON('api/author/',function(data1){
-          console.log(data1);
-          $.each(data1.authors,function (i , value){
-            if(data1.authors[i].username == global.cookie_setting.get("click_username")){
-              global.cookie_setting.set("click_first_name",data1.authors[i].first_name);
-              global.cookie_setting.set("click_last_name",data1.authors[i].last_name);
-              console.log(data1.authors[i].first_name);
-              console.log(data1.authors[i].last_name);
-              return false;
-            }
-          });
-         });
-        //console.log("target id is:"+global.cookie_setting.get("click_username"));
-        //console.log("target fist name: "+global.cookie_setting.get("click_first_name"));
-        //console.log("target last name: "+global.cookie_setting.get("click_last_name"));
         global.nav_inf.nav_inf_setting(data,page);
         global.load_posts.posts_load(data,page);
         global.button_click.clickbtn();
