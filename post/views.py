b36ea2d119c6de350db90f9cd939267952f8fb16
uaLogan <lglin@ualberta.ca>
2016-03-20 16:08:42 -0600

    increased page size

--- post/views.py
+++ post/views.py
@@ -20,7 +20,7 @@ from .permissions import *
 from remotes.utils import *
 
 class PostPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 50
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
