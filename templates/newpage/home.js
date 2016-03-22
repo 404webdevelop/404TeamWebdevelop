aa7fcd0441a4cb435d288cd28dd45347d10eee14
zuoyu <zuo2@ualberta.ca>
2016-03-22 03:27:39 -0700

    add new start page

---
+++ templates/newpage/home.js
@@ -0,0 +1,22 @@
+(function (global) {
+
+'use strict';
+
+
+
+function setusername(){
+	$("#home").hide();
+	$("#posted").hide();
+	$("#follow_btn").hide();
+	$("#other_posted").hide();
+};
+
+
+
+
+$( document ).ready(function() {
+	setusername();
+
+});
+
+})(this);
