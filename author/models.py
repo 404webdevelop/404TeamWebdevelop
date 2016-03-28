ef4296a10e82e5ce436894901e393c4bf5918b03
Yushi Wang <jasper.ys.wang@gmail.com>
2016-03-28 10:25:36 -0600

    use db to store user profile picture

--- author/models.py
+++ author/models.py
@@ -6,4 +6,4 @@ import uuid
 class Author(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     github = models.CharField(max_length=1024, blank=True, default='')
-    picture = models.ImageField(upload_to='profile_pic/', blank=True, null=True, default='')
+    picture = models.BinaryField(blank=True, null=True)
