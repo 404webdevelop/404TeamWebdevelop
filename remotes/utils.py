560316d72e15a4fbe1ae83f9281032f3f2dee5ab
uaLogan <lglin@ualberta.ca>
2016-04-03 15:47:35 -0600

    fix

--- remotes/utils.py
+++ remotes/utils.py
@@ -258,6 +258,7 @@ def PostRemoteCommentAtUrl(url, data, request, requestingUser = None):
         return 'Could not find a registered remote server corresponding to the POST url'
 
     # fill in author info from requestingUser
+    data = dict(data)
     author = {}
     author['id'] = str(requestingUser.id)
     author['host'] = request.get_host()
