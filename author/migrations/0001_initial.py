9824fe0fadc2bc24bcf3a66c6e281a42ccac06ce
uaLogan <lglin@ualberta.ca>
2016-04-03 14:24:07 -0600

    migrations fix

--- author/migrations/0001_initial.py
+++ author/migrations/0001_initial.py
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
                 ('id', models.UUIDField(default=uuid.uuid4, serialize=False, editable=False, primary_key=True)),
                 ('github', models.CharField(default=b'', max_length=1024, blank=True)),
-                ('picture', models.ImageField(default=b'', null=True, upload_to=b'profile_pic/', blank=True)),
+                ('picture', models.BinaryField(null=True, blank=True)),
                 ('groups', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups')),
                 ('user_permissions', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Permission', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions')),
             ],
