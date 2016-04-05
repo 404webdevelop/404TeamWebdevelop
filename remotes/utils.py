b5006e5ca3fe14ca6203d6b86901759c6c7816c0
uaLogan <lglin@ualberta.ca>
2016-04-04 19:19:24 -0600

    minor thing

--- remotes/utils.py
+++ remotes/utils.py
@@ -72,7 +72,8 @@ class _RemoteServer:
         if relUrl[-1] != '/':
             relUrl = relUrl + '/'
         if 'blooming' in self.host: # SPECIAL CASE
-            headers = {'Bearer': 'sh3nr<7n,G<>ku.^Kibit0-_B(?)rawww.blooming-earth-129.heroku.com'}
+            headers = {'Bearer': 'sh3nr<7n,G<>ku.^Kibit0-_B(?)ra' + 'www.blooming-earth-129.heroku.com'
+                       ,'Authorization': 'Bearer sh3nr<7n,G<>ku.^Kibit0-_B(?)ra' + 'www.blooming-earth-129.heroku.com'}
         else:
             headers = {}
         r = requests.post(self.host + relUrl, json=data, auth=self.credentials, params=self._RequestingUser(), headers=headers)
